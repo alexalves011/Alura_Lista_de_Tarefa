a6222da3baad03f70f03cb8cc015604afce8e43c
alexalves011 <alexalves.work@gmail.com>
2026-09-10 22:33:07 -0300

    Utilizando only, skip e each para controlar seus testes

--- src/components/ToDoItem/mergeClass.spec.js
+++ src/components/ToDoItem/mergeClass.spec.js
@@ -1,31 +1,10 @@
 import mergeClass from "./mergeClass";
 
 describe("./mergeClass.js", () => {
-  test("Deveria retorna a classe do todo-Item quando o item não estiver completo", () => {
-    // Arrange: preparar os dados e ambiente que precisamos para fazer o teste
-
-    const isItemCompleted = false;
-
-    // act
-
-    const styles = mergeClass(isItemCompleted);
-
-    // Assert
-
-    expect(styles).toBe("todo-item");
-  });
-
-  test("Deveria retorna a classe do todo-Item quando o item não estiver completo", () => {
-    // Arrange: preparar os dados e ambiente que precisamos para fazer o teste
-
-    const isItemCompleted = true;
-
-    // act
-
-    const styles = mergeClass(isItemCompleted);
-
-    // Assert
-
-    expect(styles).toBe("todo-item completed");
+  test.each([
+    { entrada: true, saida: "todo-item completed" },
+    { entrada: false, saida: "todo-item" },
+  ])("deveria retornar a classe base do todo-item e a classe completed quando o item e...", ({entrada, saida}) => {
+    expect(mergeClass(entrada)).toBe(saida);
   });
 });
