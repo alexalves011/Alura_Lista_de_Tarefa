ada335c4c86c16aa05f87d802a26eeb9ef15fde6
alexalves011 <alexalves.work@gmail.com>
2026-09-22 16:46:51 -0300

    test: adiciona testes para o componente SubHeading

--- src/components/SubHeading/index.spec.jsx
+++ src/components/SubHeading/index.spec.jsx
@@ -2,64 +2,28 @@ import React from "react";
 import { SubHeading } from ".";
 import { render } from "@testing-library/react";
 
+describe("SubHeading", () => {
+  describe("getBy", () => {
+    test("deveria renderizar o componente diretamente", () => {
+      const { getByText } = render(<SubHeading>Para Estudar</SubHeading>);
+      expect(getByText("Para Estudar")).toBeInTheDocument();
+    });
 
-
-describe('SubHeading', () => {
-
-
-describe("getBy", () =>{
-
-
-   test('deveria redenizar o componente diretamente', () => {
-
-    const {getByText} = render(<SubHeading>Para Estudar</SubHeading>)
-    expect(getByText("Para Estudar")).toBeInTheDocument();
-    
+    test.skip("não deveria renderizar o componente quando não tem children", () => {
+      const { getByText } = render(<SubHeading></SubHeading>);
+      expect(getByText("Para Estudar")).toBeNull();
+    });
   });
 
+  describe("queryBy", () => {
+    test("deveria renderizar o componente diretamente", () => {
+      const { queryByText } = render(<SubHeading>Para Estudar</SubHeading>);
+      expect(queryByText("Para Estudar")).toBeInTheDocument();
+    });
 
-
-  test.skip('não deveria redenizar o componente quando não tem children', () => {
-
-    const {getByText} = render(<SubHeading></SubHeading>)
-    expect(getByText("Para Estudar")).toBeNull();
-    
+    test("não deveria renderizar o componente quando não tem children", () => {
+      const { queryByText } = render(<SubHeading></SubHeading>);
+      expect(queryByText("Para Estudar")).toBeNull();
+    });
   });
-
-
-
-}) 
-
-
- 
-
-
-describe("querryBy", () => {
-
-
-
-  test('deveria redenizar o componente diretamente', () => {
-
-    const {queryByText} = render(<SubHeading>Para Estudar</SubHeading>)
-    expect(queryByText("Para Estudar")).toBeInTheDocument();
-    
-  });
-
-
-
-  test('não deveria redenizar o componente quando não tem children', () => {
-
-    const {queryByText} = render(<SubHeading></SubHeading>)
-    expect(queryByText("Para Estudar")).toBeNull();
-    
-  });
-
-
-
-
-})
-
-
-
-
 });
